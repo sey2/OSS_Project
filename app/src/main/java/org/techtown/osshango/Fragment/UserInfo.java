c3e21ca0ed6b15f1dc98a6abd09847b5ab5acfc1
sey2 <casio2978@naver.com>
2023-01-06 01:58:36 +0900

    [Update] User 정보 화면, Crop Image (23.01.06)

--- app/src/main/java/org/techtown/osshango/Fragment/UserInfo.java
+++ app/src/main/java/org/techtown/osshango/Fragment/UserInfo.java
@@ -1,66 +1,163 @@
 package org.techtown.osshango.Fragment;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
+
 
 import org.techtown.osshango.R;
+import org.techtown.osshango.ViewModel.TravelViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link UserInfo#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.InputStream;
+
+
 public class UserInfo extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private TravelViewModel userModel;
+    private ImageView profile;
+    private TextView mbtiTextView;
+    private TextView nameTextView;
+    private Button changImageBtn;
+    private ProgressBar expBar;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public UserInfo() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment UserInfo.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static UserInfo newInstance(String param1, String param2) {
-        UserInfo fragment = new UserInfo();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    private static final int REQUEST_CODE = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_info, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_user_info, container, false);
+
+        userModel = new ViewModelProvider(getActivity()).get(TravelViewModel.class);
+        profile = root.findViewById(R.id.home_profile);
+        mbtiTextView = root.findViewById(R.id.mbtiTextView);
+        nameTextView = root.findViewById(R.id.nameTextView);
+        changImageBtn = root.findViewById(R.id.changeImageBtn);
+        expBar = root.findViewById(R.id.expBar);
+
+        // 사용자 프로필을 불러온다.
+        Glide.with(getContext()).load(userModel.getUserinfo().getValue().getUserProfile()).into(profile);
+
+        // 사용자 Mbti, 이름 설정
+        mbtiTextView.setText(userModel.getUserinfo().getValue().getUserMbti());
+        nameTextView.setText(userModel.getUserinfo().getValue().getUserName() + "님");
+
+        changImageBtn.setOnClickListener((v) -> {
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.putExtra("crop",true);
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+
+            startActivityForResult(intent, 104);
+        });
+
+        expBar.setProgress(75);
+
+        setProfileFromCloud();
+
+        return root;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        switch (requestCode) {
+            case 104:  // 사진을 앨범에서 선택하는 경우
+                if(resultCode == getActivity().RESULT_OK) {
+                    Uri imgUri = intent.getData();
+                    cloudUploadImg(imgUri);     // 클라우드에 이미지 저장
+                    userModel.getUserinfo().getValue().setProfileUri(imgUri);
+                    CropImage.activity(imgUri).setGuidelines(CropImageView.Guidelines.ON).start(getContext(), this);
+                }
+
+                break;
+
+            /* 자른 사진을 pictureImageView에 적용 */
+            case CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE:
+                CropImage.ActivityResult result = CropImage.getActivityResult(intent);
+                if (result != null) {
+                    Uri resultUri = result.getUri();
+
+                    ContentResolver resolver = getContext().getContentResolver();
+
+                    try {
+                        InputStream instream = resolver.openInputStream(resultUri);
+                        Bitmap resultPhotoBitmap = BitmapFactory.decodeStream(instream);
+
+                        //resultPhotoBitmap = getRoundedCornerBitmap(resultPhotoBitmap, 20);
+                        profile.setImageBitmap(resultPhotoBitmap);
+
+                        instream.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    break;
+                }
+
+        }
+    }
+
+    public void cloudUploadImg(Uri imgUri){
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        String fileName = userModel.getUserinfo().getValue().getUserID() +"profile.png";
+        StorageReference storageRef = storage.getReference("profile/" + fileName);
+
+        UploadTask uploadTask = storageRef.putFile(imgUri);
+        uploadTask.addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                Log.d("test", "sucess");
+            }
+        });
+    }
+
+    private void setProfileFromCloud(){
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference imgRef = storageRef.child("profile/" + userModel.getUserinfo().getValue().getUserID() +"profile.png");
+
+        if(imgRef != null){
+            imgRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                @Override
+                public void onSuccess(Uri uri) {
+                    Glide.with(getContext()).load(uri).into(profile);
+                }
+
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("test", e.toString());
+                }
+            });
+        }
     }
 }
